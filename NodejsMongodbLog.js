ede82709a80fc22c4d62716bb3167e667ceea312
Tracy <1002173040@qq.com>
2021-12-12 17:26:46 -0800

    mongodb success from

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,7 +1,7 @@
 const express =  require("express")
 const app=express()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test1');
+mongoose.connect('mongodb://172.21.2.236:27017/190110910216');
 const schema={
     name:String,
     age:Number,
@@ -9,12 +9,14 @@ const schema={
     hobby:String
 }
 const mybeta = mongoose.model('cat1s', schema);
-const kitty = new mybeta({ name: 'testZildjian2' });
-kitty.save()
+// const kitty = new mybeta({ name: 'testZildjian2' });
+// kitty.save()
 
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
     console.log(req.query)
+    const kitty = new mybeta({ name: req.query.first,health:req.query.second });
+    kitty.save()
 })
 app.listen(1216)
